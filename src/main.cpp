1fe3b707a6233fab43a31e6ba77e022941ade829
Hatsalesman <Isac.sjoback@gmail.com>
2025-10-22 16:03:55 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -54,7 +54,7 @@ static void create_ui() {
   // Tile #1
   {
     t1_label = lv_label_create(t1);
-    lv_label_set_text(t1_label, "Hello Students");
+    lv_label_set_text(t1_label, "Group 15, ver. 1.0");
     lv_obj_set_style_text_font(t1_label, &lv_font_montserrat_28, 0);
     lv_obj_center(t1_label);
     apply_tile_colors(t1, t1_label, /*dark=*/false);
