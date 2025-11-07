42b5128b2989c97a120ab4ed35bafbc78baf3aa3
barbariand <cindy@simd.me>
2025-11-07 13:09:33 +0100

    Fix: header Widget: Component

---
+++ lib/gui/GUI.hpp
@@ -0,0 +1,20 @@
+#pragma once
+// Include core types first
+#include "./types/Color.hpp"
+#include "./types/Enums.hpp"
+#include "./types/StyleSelector.hpp"
+
+// Include core classes
+#include "./core/BaseWidget.hpp"
+#include "./core/Component.hpp"
+#include "./core/Screen.hpp"
+#include "./core/Widget.hpp"
+
+// Include the mixins
+#include "./widgets/mixins/TextProperties.hpp"
+
+// Include all widgets
+#include "./widgets/Button.hpp"
+#include "./widgets/Label.hpp"
+#include "./widgets/Tile.hpp"
+#include "./widgets/Tileview.hpp"
