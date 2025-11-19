6a3975f71ac85ab9e7c5d9025020c3903edf5050
ZebbZ-Z <71083192+ZebbZ-Z@users.noreply.github.com>
2025-11-19 12:15:55 +0100

    WiFi Connection Module
    Wifi connection module med felhantering

--- src/wifi.h
+++ src/wifi.h
@@ -2,3 +2,7 @@
 // Wi-Fi credentials (Delete these before commiting to GitHub)
 static const char* WIFI_SSID = "SSID";
 static const char* WIFI_PASSWORD = "PWD";
+
+bool connect_wifi();
+
+void wifi_reconnect_backoff();
