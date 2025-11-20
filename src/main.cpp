cab8c69065b9967d2f74307e8475cb851fdd1a07
barbariand <cindy@simd.me>
2025-11-20 16:27:33 +0100

    fixing main conflicts

--- src/main.cpp
+++ src/main.cpp
@@ -13,81 +13,8 @@ void handle_sigterm(int signum) {
 }
 #endif
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-// Function: Tile #2 Color change
-static void apply_tile_colors(lv_obj_t* tile, lv_obj_t* label, bool dark) {
-  std::cout << "lets goooooooo changing colour" << std::endl;
-  // Background
-  lv_obj_set_style_bg_opa(tile, LV_OPA_COVER, 0);
-  lv_obj_set_style_bg_color(tile, dark ? lv_color_black() : lv_color_white(),
-                            0);
 
-  // Text
-  lv_obj_set_style_text_color(label, dark ? lv_color_white() : lv_color_black(),
-                              0);
-}
-
-static void on_tile2_clicked(lv_event_t* e) {
-  LV_UNUSED(e);
-  t2_dark = !t2_dark;
-  apply_tile_colors(t2, t2_label, t2_dark);
-}
-
-// Function: Creates UI
-static void create_ui() {
-  // Fullscreen Tileview
-  tileview = lv_tileview_create(lv_scr_act());
-  lv_obj_set_size(tileview, lv_disp_get_hor_res(NULL),
-                  lv_disp_get_ver_res(NULL));
-  lv_obj_set_scrollbar_mode(tileview, LV_SCROLLBAR_MODE_OFF);
-
-  // Add two horizontal tiles
-  t1 = lv_tileview_add_tile(tileview, 0, 0, LV_DIR_HOR);
-  t2 = lv_tileview_add_tile(tileview, 1, 0, LV_DIR_HOR);
-
-  // Tile #1
-  {
-    t1_label = lv_label_create(t1);
-<<<<<<< HEAD
-    lv_label_set_text(t1_label, "Group 15, ver. 1.0");
-=======
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    lv_label_set_text(t1_label, "Group 15, ver. 1.0");
-=======
-    lv_label_set_text(t1_label, "I enjoy the weather!");
->>>>>>> Stashed changes
-=======
-    lv_label_set_text(t1_label, "I enjoy the weather!");
->>>>>>> Stashed changes
->>>>>>> parent of dd4cf97 (Merge pull request #5 from barbariand/gui)
-    lv_obj_set_style_text_font(t1_label, &lv_font_montserrat_28, 0);
-    lv_obj_center(t1_label);
-    apply_tile_colors(t1, t1_label, /*dark=*/false);
-  }
-
-  // Tile #2
-  {
-    t2_label = lv_label_create(t2);
-    lv_label_set_text(t2_label, "Welcome to the workshop");
-    lv_obj_set_style_text_font(t2_label, &lv_font_montserrat_28, 0);
-    lv_obj_center(t2_label);
-
-    apply_tile_colors(t2, t2_label, /*dark=*/false);
-    lv_obj_add_flag(t2, LV_OBJ_FLAG_CLICKABLE);
-    lv_obj_add_event_cb(t2, on_tile2_clicked, LV_EVENT_CLICKED, NULL);
-  }
-}
-
-// Function: Connects to WIFI
 static void connect_wifi() {
-=======
-void connect_wifi() {
->>>>>>> f0a8c8361958a75547535775673c8d9f5626f6d9
-=======
-void connect_wifi() {
->>>>>>> f0a8c8361958a75547535775673c8d9f5626f6d9
   // Serial.printf("Connecting to WiFi SSID: %s\n", WIFI_SSID);
   // WiFi.mode(WIFI_STA);
   // WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
