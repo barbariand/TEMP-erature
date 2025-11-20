0c8ad86f1f2f11882386e9f4c6fa2cd0ba10c57f
chbl24 <chbl24@student.bth.se>
2025-11-20 14:22:50 +0100

    Merge pull request #5 from barbariand/gui
    Gui LVGL Wrapper

--- src/main.cpp
+++ src/main.cpp
@@ -55,6 +55,9 @@ static void create_ui() {
   // Tile #1
   {
     t1_label = lv_label_create(t1);
+<<<<<<< HEAD
+    lv_label_set_text(t1_label, "Group 15, ver. 1.0");
+=======
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
     lv_label_set_text(t1_label, "Group 15, ver. 1.0");
@@ -64,6 +67,7 @@ static void create_ui() {
 =======
     lv_label_set_text(t1_label, "I enjoy the weather!");
 >>>>>>> Stashed changes
+>>>>>>> parent of dd4cf97 (Merge pull request #5 from barbariand/gui)
     lv_obj_set_style_text_font(t1_label, &lv_font_montserrat_28, 0);
     lv_obj_center(t1_label);
     apply_tile_colors(t1, t1_label, /*dark=*/false);
