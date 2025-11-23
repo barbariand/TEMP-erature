5d60e5164f2c0aca5d492d9c956ebb29d81a8993
chbl24 <chbl24@student.bth.se>
2025-11-23 13:41:45 +0100

    deleted the extra wifi cred

--- src/wifi_cred.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-// Wi-Fi credentials (Delete these before commiting to GitHub)
-static const char* WIFI_SSID = "Balkow";
-static const char* WIFI_PASSWORD = "Skibidi132";
-static const char* WIFI_SSID = "BTH_Guest";
-static const char* WIFI_PASSWORD = "oliv95lila";
