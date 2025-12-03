0344ce385263d677194ee1aae9a6aad5b2fe0712
barbariand <cindy@simd.me>
2025-12-03 21:32:38 +0100

    Reimplementing MainUi

---
+++ src/temp_gui/tiles/MainUi.cpp
@@ -0,0 +1,17 @@
+#include "MainUi.hpp"
+
+using namespace LVGL_Wrapper;
+
+MainUi::MainUi(BaseWidget* parent) : Component(parent) {}
+
+void MainUi::init() {
+  set_size(LV_PCT(100), LV_PCT(100));
+  set_style_bg_color(Color::White);
+  set_style_bg_opa(Opa::Cover);
+
+  m_label = Label::create(*this);
+  m_label->set_text("Group 15\nVersion 1.0")
+      .set_style_text_font(&lv_font_montserrat_28)
+      .set_style_text_align(LV_TEXT_ALIGN_CENTER)
+      .center();
+}
